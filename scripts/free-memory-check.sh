145f880d39ca081a60ba23d1e91b947cf13bc931
paula-zaki <paula.mamdouh.zaki@gmail.com>
2026-08-13 02:55:35 +0300

    Create free-memory-check.sh

---
+++ scripts/free-memory-check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env
+
+# Slack webhook to get the notification
+SLACK_WEBHOOK=""
+
+# the amount of free memory in the system
+FREE_MEMORY=$(vmstat -S m | awk 'NR==3 {print $4 }')
+
+# Message when the memory is leaking
+
+# cheking if it is less than 100M
+if [ $FREE_MEMORY -lt 100 ]
+then
+        curl -X POST \ 
+        -H "Content-Type: application/json" \
+        --data "{\"text\":\"$MESSAGE\"}"\
+        $SLACK_WEBHOOK
+fi
