e7249ad5db84088d60fac160b5220cd8a03cce59
paula-zaki <paula.mamdouh.zaki@gmail.com>
2026-08-13 03:37:33 +0300

    Create backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Remote server details
+REMOTE_USER=vagrant
+REMOTE_HOST=
+REMOTE_DIR=/backup/
+
+# Source directory
+SOURCE_DIR=/home/vagrant
+
+# Rsync command for secure backup
+
+rsync -avz -e ssh $SOURCE_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR >> /tmp/backup.log 2>&1
+
+
+# Error checking
+
+if [ $? -eq 0 ]
+then
+        echo 'Backup is Successful'
+else
+        echo 'Backup failed'
+        exit
+fi
